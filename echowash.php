6e270add602265b9c7bdae26202bc780ed9aa75f
MeikoDis <meikodis@meikodis.org>
2013-02-20 21:22:22 +0100

    Client thing

---
+++ echowash.php
@@ -0,0 +1,15 @@
+<?php
+
+#################################
+# Waschi Waschmaschinenverbund  #
+# Version: 0.4-0002             #
+# (c) 2013 by MeikoDis          #
+# License: GNU-AGPL v3          #
+#################################
+
+
+# May used for clients
+
+	include("wash.php");
+	echo($status);
+?>
