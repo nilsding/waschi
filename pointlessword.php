dbf69c23ce588a8a366a13863cbb2fc5cd3a2133
nilsding <nilsding@myopera.com>
2013-03-01 23:10:52 +0100

    Checks if pointless word is filtered

--- pointlessword.php
+++ pointlessword.php
@@ -1,7 +1,13 @@
 <?php
 
-# by Revengeday
+# PointlessWords by Revengeday
+# Waschi-Dings by dr. nilsding
 
-$word = file_get_contents("http://dev.revengeday.de/pointlesswords/api/");
+include_once("filter.php");
+
+$word = "PointlessWords";
+do {
+	$word = file_get_contents("http://dev.revengeday.de/pointlesswords/api/");
+} while (in_filter($word));
 echo html_entity_decode($word);
 ?>
