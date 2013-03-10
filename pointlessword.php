10c7e5facb57bcad2d26659cbfac15a718cb5828
Seldan <register@seldan.de>
2013-03-10 17:54:32 +0100

    Now working with PHP 5.3 again.
    Accidently used ENT_HTML5, which was introduced in PHP5.4 and broke backwards compatibility. This has been changed to ENT_COMPAT now. I also downloaded PHP5.3 and tested the script there, to make sure any issues have been resolved.

--- pointlessword.php
+++ pointlessword.php
@@ -7,7 +7,7 @@ include_once("filter.php");
 
 $word = "PointlessWords";
 do {
-	$word = html_entity_decode(file_get_contents("http://dev.revengeday.de/pointlesswords/api/"), ENT_HTML5, "UTF-8");
+	$word = html_entity_decode(file_get_contents("http://dev.revengeday.de/pointlesswords/api/"), ENT_COMPAT, "UTF-8");
 } while (in_filter($word));
 echo $word;
 ?>
