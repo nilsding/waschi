ae72f2ffe194d395b19c1985f685668e3665e1fd
Seldan <register@seldan.de>
2013-03-10 16:32:39 +0100

    Fixed pointlessword filtering and encoding.
    -PHP prior to version 5.4.0 defaults to ISO-8859-1, enforce UTF-8, the whole site uses UTF-8.
    -The input was filtered before the html entities were converted, this filtered out all strings with html entities, this is now fixed by calling html_entity_decode() before in_filter().

--- pointlessword.php
+++ pointlessword.php
@@ -7,7 +7,7 @@ include_once("filter.php");
 
 $word = "PointlessWords";
 do {
-	$word = file_get_contents("http://dev.revengeday.de/pointlesswords/api/");
+	$word = html_entity_decode(file_get_contents("http://dev.revengeday.de/pointlesswords/api/"), ENT_HTML5, "UTF-8");
 } while (in_filter($word));
-echo html_entity_decode($word);
+echo $word;
 ?>
